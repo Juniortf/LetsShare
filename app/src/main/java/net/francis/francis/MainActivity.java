a6a5a349a842905cfa3e0ce61ccc3bc812e4bd99
Francis <juniorfrancist@gmail.com>
2017-02-23 13:17:38 +0300

    first commit

---
+++ app/src/main/java/net/francis/francis/MainActivity.java
@@ -0,0 +1,51 @@
+package net.francis.francis;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText userName;
+    EditText password;
+    TextView dataView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userName = (EditText) findViewById(R.id.userNameInput);
+        password = (EditText) findViewById(R.id.passwordInput);
+        dataView = (TextView) findViewById(R.id.dataTextView);
+
+    }
+
+    public void saveData(View view){
+        SharedPreferences loginData = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = loginData.edit();
+        editor.putString("userName", userName.getText().toString());
+        editor.putString("password", password.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this,"Saved", Toast.LENGTH_LONG).show();
+        userName.setText("");
+        password.setText("");
+        dataView.setText("");
+
+    }
+
+    public void getData(View view){
+        SharedPreferences loginData = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String name = loginData.getString("userName", "");
+        String pw = loginData.getString("password","");
+        String msg = "Saved User Name: " + name + "\nSaved Password: " + pw;
+        dataView.setText(msg);
+    }
+}
+
